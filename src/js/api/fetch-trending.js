27b6257a9cb54e8004bb048bedc91fc2d258b2c7
Roman Serediuk <box4rom@gmail.com>
2022-08-22 23:34:35 +0300

    fetch-trending update

--- src/js/api/fetch-trending.js
+++ src/js/api/fetch-trending.js
@@ -1,7 +1,7 @@
 export const fetchTrending = async function (page = 1) {
   const API_KEY = 'd70849b39c7b399ded2dffef6ee1baa4';
   const response = await fetch(
-    `https://api.themoviedb.org/3/trending/all/day?&api_key=${API_KEY}&page=${page}`
+    `https://api.themoviedb.org/3/trending/all/week?&api_key=${API_KEY}&page=${page}`
   );
   const data = await response.json();
   return data;
--- src/js/home/parseTrending.js
+++ src/js/home/parseTrending.js
@@ -1,4 +1,8 @@
 import { fetchTrending } from '../api/fetch-trending';
+// import parseGallery from '../tmp/parse_gallery.hbs';
+const galleryListEl = document.querySelector('.gallery__list');
 
 let page;
-fetchTrending(page).then(console.log);
+fetchTrending(page).then(
+  data => (galleryListEl.innerHTML = parseGallery(data))
+);
