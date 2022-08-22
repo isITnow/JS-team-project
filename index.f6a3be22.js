c72f2ac3a5c7ba8e0a36c6d14f9c9521cbcdcae3
isITnow <104834083+isITnow@users.noreply.github.com>
2022-08-22 18:10:14 +0000

    Deploying to gh-pages from @ isITnow/JS-team-project@12e0f26578be9678b9b25af5a27b85431f889e92 🚀

---
+++ index.f6a3be22.js
@@ -0,0 +1,2 @@
+(async function(e=1){const a=await fetch(`https://api.themoviedb.org/3/trending/all/day?&api_key=d70849b39c7b399ded2dffef6ee1baa4&page=${e}`);return await a.json()})(undefined).then(console.log);
+//# sourceMappingURL=index.f6a3be22.js.map
