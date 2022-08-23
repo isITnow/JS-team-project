c6fa97aa2ce4ed1ed33abd2d86f52363570020d0
Heldenglanz <heldenglanz@gmail.com>
2022-08-23 14:48:17 +0300

    omg render

--- src/js/api/filmsByPopular.js
+++ src/js/api/filmsByPopular.js
@@ -1,36 +1,9 @@
 import { fetchTrending } from './fetch';
-const galleryItem = document.querySelector('.gallery__list');
+import {fetchGenres} from './fetch'
+import { renderGalleryMarkup } from '../home/renderGalleryMarkup';
 
-fetchTrending(1).then(data => filmByPopular(data.results));
+let page = 1;
+fetchGenres()
+.then(data=> (localStorage.setItem('genresData', JSON.stringify(data.genres))))
+fetchTrending(page).then(data => renderGalleryMarkup(data.results));
 
-export function filmByPopular(arr) {
-  const markup = arr.reduce(
-    (
-      acc,
-      { id, poster_path, name, title, release_date, vote_average, genre_ids }
-    ) => {
-      const currentGenres = [];
-      const genres = JSON.parse(localStorage.getItem(`genresList`));
-      genres.forEach(elem => {
-        if (genre_ids.includes(elem.id)) {
-          currentGenres.push(elem.name);
-        }
-      });
-
-      return (
-        acc +
-        `<li class="gallery__item" id=${id}>
-            <img class="gallery__image" src="https://image.tmdb.org/t/p/original${poster_path}" alt="${
-          title || name
-        }" width="100"/>
-            <h2 class="gallery__title">${title || name}</h2>
-            <p>${currentGenres.join(',')}</p>
-                <p class="gallery__release">${release_date.slice(0, 4)}</p>
-                <span class="gallery__rating">${vote_average.toFixed(1)}</span>
-            </li>`
-      );
-    },
-    ''
-  );
-  galleryItem.innerHTML = markup;
-}
