77b51f367789bb54d59ddac7eefb82edaea9ce09
Heldenglanz <heldenglanz@gmail.com>
2022-08-23 10:19:53 +0300

    tr

--- src/js/api/fetch.js
+++ src/js/api/fetch.js
@@ -1,15 +1,36 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'api_key=d70849b39c7b399ded2dffef6ee1baa4';
 
-export function fetchMovies(name){
-    const search = new URLSearchParams({
-        fields: 'name,backdrop_path,genres,release_date,vote_average,poster_path',
-    });
+// export function fetchMovies(name){
+//     const search = new URLSearchParams({
+//         fields: 'name,backdrop_path,genres,release_date,vote_average,poster_path',
+//     });
     
-    return fetch(`${BASE_URL}/movie/${id}?${API_KEY}&append_to_response=videos${search}`).then(response => {
-          if(!response.ok){
-            throw new Error(response.status);
-          }
-            return response.json();
-        });
-      }
+//     return fetch(`${BASE_URL}/movie/${id}?${API_KEY}&append_to_response=videos${search}`).then(response => {
+//           if(!response.ok){
+//             throw new Error(response.status);
+//           }
+//             return response.json();
+//         });
+//       }
+      export function fetchByKey(search){
+        const search = keyword_id
+      return fetch(`${BASE_URL}/3/keyword/${search}?api_key=${API_KEY}`).then(response => {
+        if(!response.ok){
+          throw new Error(response.status);
+        }
+          return response.json();
+      });
+    }
+  // поиск по слову
+
+
+    export function fetchPopular(search){
+      return fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`).then(response => {
+        if(!response.ok){
+          throw new Error(response.status);
+        }
+          return response.json();
+      });
+    }
+// отдаст массив фильмов в порядке убьівания по рейтингу
