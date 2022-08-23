e576ee56f6aaa0d4a675e0e5920d0775d2af87fa
isITnow <104834083+isITnow@users.noreply.github.com>
2022-08-23 08:10:07 +0000

    Deploying to gh-pages from @ isITnow/JS-team-project@3696e648f56c7c9b74e4a35d9825315a0dc933a1 🚀

---
+++ index.2f16d0f3.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery__list");
+//# sourceMappingURL=index.2f16d0f3.js.map
---
+++ index.f92f4b44.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery__list");
+//# sourceMappingURL=index.f92f4b44.js.map
