4f5a765ead2e330ac22fa7af0a176e8d2f8e4a07
Roman Serediuk <box4rom@gmail.com>
2022-08-23 09:53:26 +0300

    fetchTrending update

--- src/js/api/fetch-trending.js
+++ src/js/api/fetch-trending.js
@@ -1,8 +1,11 @@
-export const fetchTrending = async function (page = 1) {
-  const API_KEY = 'd70849b39c7b399ded2dffef6ee1baa4';
-  const response = await fetch(
-    `https://api.themoviedb.org/3/trending/all/week?&api_key=${API_KEY}&page=${page}`
-  );
-  const data = await response.json();
-  return data;
-};
+// export const fetchTrending = function (page = 1) {
+//   const API_KEY = 'd70849b39c7b399ded2dffef6ee1baa4';
+//   return fetch(
+//     `https://api.themoviedb.org/3/trending/movie/week?&api_key=${API_KEY}&page=${page}`
+//   ).then(resp => {
+//     if (!resp.ok) {
+//       throw new Error(resp.status);
+//     }
+//     return resp.json();
+//   });
+// };
