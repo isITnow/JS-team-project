3696e648f56c7c9b74e4a35d9825315a0dc933a1
Roman Serediuk <box4rom@gmail.com>
2022-08-23 11:09:06 +0300

    hbs uninstall

--- src/js/home/parseTrending.js
+++ src/js/home/parseTrending.js
@@ -1,11 +1,10 @@
 import { fetchTrending } from '../api/fetch';
-import parseGallery from '../tmp/parse_gallery.hbs';
 const galleryListEl = document.querySelector('.gallery__list');
 
-let page;
-fetchTrending(page).then(data => createMarkup(data));
+// let page;
+// fetchTrending(page).then(data => createMarkup(data));
 
-function createMarkup(data) {
-  console.log(data.results);
-  galleryListEl.innerHTML = parseGallery(data.results);
-}
+// function createMarkup(data) {
+//   console.log(data.results);
+//   galleryListEl.innerHTML = parseGallery(data.results);
+// }
