1c88321c94796c300c372f0260cd845aed3cc80e
Roman Serediuk <box4rom@gmail.com>
2022-08-24 20:23:49 +0300

    Update localStorage.js

--- src/js/library/localStorage.js
+++ src/js/library/localStorage.js
@@ -1,23 +1,14 @@
 // const addToWatchedBtn = document.querySelector('.card-modal__button-add');
 // const watchedArr = [];
 
-// // console.log('TEST');
-
-// addToWatchedBtn.addEventListener('click', onAddToWatchedClick);
-// function onAddToWatchedClick(evt) {
-//   //   if (localStorage.getItem('watchedMovies')) {
-//   //     return;
-//   //     }
-//   console.log(evt);
-// }
-
-// localStorage.setItem('watchedMovies', JSON.stringify('watchedArr'));
-
 function addWatchedToLocalStorage(data) {
-  if (localStorage.getItem('watchedMovies')) {
+  if (!localStorage.getItem('watchedMovies')) {
+    const watchedArr = [];
+    watchedArr.push(data);
+    localStorage.setItem('watchedMovies', JSON.stringify('watchedArr'));
     return;
   }
-  const watchedArr = [];
-  watchedArr.push(data);
-  localStorage.setItem('watchedMovies', JSON.stringify('watchedArr'));
+  const arr = JSON.parse(localStorage.getItem('watchedMovies'));
+  // arr.push(data);
+  localStorage.setItem('watchedMovies', JSON.stringify(arr.push(data)));
 }
