586855e37d945695649807dfd6ca2bc1b5a861c4
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-25 10:02:07 +0300

    fix eventlistner in modal

--- src/js/modal/modal-footer.js
+++ src/js/modal/modal-footer.js
@@ -18,16 +18,18 @@ import { renderMovieMarkup } from '../renderModal';
   function onOpenModal() {
     refs.modal.classList.remove('is-hidden');
     window.addEventListener('keydown', onEscapeCloseModal);
+    window.addEventListener('click', onBackdropClickCloseModal);
   }
 
   function onCloseModal() {
     refs.modal.classList.add('is-hidden');
+    window.removeEventListener('keydown', onEscapeCloseModal);
+    window.removeEventListener('click', onBackdropClickCloseModal);
   }
 
   function onEscapeCloseModal(event) {
     if (event.code === 'Escape') {
       refs.modal.classList.add('is-hidden');
-      window.removeEventListener('keydown', onEscapeCloseModal);
       onCloseModal();
     }
   }
@@ -65,27 +67,22 @@ function onOpenCardModal(event) {
   }
   fetchByID(id).then(data => renderMovieMarkup(data));
   refs.cardModal.classList.remove('is-hidden');
-  addEventListener();
-  // window.addEventListener('keydown', onEscapeCloseModal);
-  // window.addEventListener('click', onBackdropClickCloseModal);
-  // window.addEventListener('click', onAddToWatched);
-  // window.addEventListener('click', onAddToQueue);
+
+  window.addEventListener('keydown', onEscapeCloseModal);
+  window.addEventListener('click', onBackdropClickCloseModal);
 }
 
 function onCloseCardModal() {
   refs.cardModal.classList.add('is-hidden');
   refs.modalContainer.innerHTML = '';
-  removeEventListener();
-  // window.removeEventListener('keydown', onEscapeCloseModal);
-  // window.removeEventListener('click', onBackdropClickCloseModal);
-  // window.removeEventListener('click', onAddToWatched);
-  // window.removeEventListener('click', onAddToQueue);
+
+  window.removeEventListener('keydown', onEscapeCloseModal);
+  window.removeEventListener('click', onBackdropClickCloseModal);
 }
 
 function onEscapeCloseModal(event) {
   if (event.code === 'Escape') {
     onCloseCardModal();
-    window.removeEventListener('keydown', onEscapeCloseModal);
   }
 }
 
@@ -94,6 +91,7 @@ function onBackdropClickCloseModal(event) {
     onCloseCardModal();
   }
 }
+<<<<<<< Updated upstream
 function onAddToWatched(event) {
   if (event.currentTarget === event.target) {
     console.log('ok1');
@@ -119,3 +117,18 @@ function removeEventListener() {
   window.removeEventListener('click', onAddToWatched);
   window.removeEventListener('click', onAddToQueue);
 }
+=======
+// function onAddToWatched(event) {
+//   if (event.currentTarget === event.target) {
+//     console.log('ok1');
+//     event.target.textContent = 'REMOVE';
+//   }
+// }
+
+// function onAddToQueue(event) {
+//   if (event.currentTarget === event.target) {
+//     console.log('ok2');
+//     event.target.textContent = 'REMOVE';
+//   }
+// }
+>>>>>>> Stashed changes
