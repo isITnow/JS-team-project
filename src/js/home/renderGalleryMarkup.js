e8b9ca38a674cd9c339f31dccc379843f2d4e81e
Roman Serediuk <box4rom@gmail.com>
2022-08-25 20:02:58 +0300

    crazy magic

--- src/js/home/renderGalleryMarkup.js
+++ src/js/home/renderGalleryMarkup.js
@@ -1,6 +1,8 @@
 const galleryItem = document.querySelector('.gallery__list');
 
-export function renderGalleryMarkup(results, page, total_pages) {
+export function renderGalleryMarkup(page, results, total_pages, dataAttribute) {
+  console.log(page);
+  console.log(results);
   const markup = results.reduce(
     (
       acc,
@@ -19,7 +21,7 @@ export function renderGalleryMarkup(results, page, total_pages) {
         : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
       return (
         acc +
-        `<li class="gallery__item" id=${id} data-trending>
+        `<li class="gallery__item" id=${id} ${dataAttribute}>
           <div class="gallery__link">
               <img class="gallery__image" src="${poster_path}" alt="${
           title || name
@@ -46,149 +48,199 @@ export function renderGalleryMarkup(results, page, total_pages) {
     ''
   );
   galleryItem.innerHTML = markup;
-  return { results, page, total_pages };
+  console.log(dataAttribute);
+  return { page, results, total_pages, dataAttribute };
 }
 
-export function renderQueryMarkup(results, page, total_pages) {
-  const markup = results.reduce(
-    (
-      acc,
-      { id, poster_path, name, title, release_date, vote_average, genre_ids }
-    ) => {
-      const currentGenres = [];
-      const genres = JSON.parse(localStorage.getItem('genresData'));
-      genres.forEach(item => {
-        if (genre_ids.includes(item.id)) {
-          currentGenres.push(item.name);
-        }
-      });
-      poster_path = poster_path
-        ? 'https://image.tmdb.org/t/p/original' + poster_path
-        : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
-      return (
-        acc +
-        `<li class="gallery__item" id=${id} data-query>
-          <div class="gallery__link">
-              <img class="gallery__image" src="${poster_path}" alt="${
-          title || name
-        }" width="394" height ="335"/> 
-        </div>
-        <div class="gallery__info">
-              <h2 class="gallery__title">${title || name}</h2>
-              <p class="gallery__genres">
-              <span class="gallery__genr"> ${
-                currentGenres.length > 3
-                  ? currentGenres.splice(0, 2).join(', ') + ', Others'
-                  : currentGenres.join(', ') || 'genre information missing'
-              } </span> |
-                  <span class="gallery__release">${
-                    release_date ? release_date.slice(0, 4) : 'no release date'
-                  }</span>
-                  <span class="gallery__rating">${vote_average.toFixed(1)}
-              </span>
-              </p>
-          </div>
-        </li>`
-      );
-    },
-    ''
-  );
-  galleryItem.innerHTML = markup;
-  return { results, page, total_pages };
-}
+// export function renderGalleryMarkup(results, page, total_pages) {
+//   const markup = results.reduce(
+//     (
+//       acc,
+//       { id, poster_path, name, title, release_date, vote_average, genre_ids }
+//     ) => {
+//       const currentGenres = [];
+//       const genres = JSON.parse(localStorage.getItem('genresData'));
+//       genres.forEach(item => {
+//         if (genre_ids.includes(item.id)) {
+//           currentGenres.push(item.name);
+//         }
+//       });
 
-export function renderWatchedMarkup(results, page, total_pages) {
-  const markup = results.reduce(
-    (
-      acc,
-      { id, poster_path, name, title, release_date, vote_average, genre_ids }
-    ) => {
-      const currentGenres = [];
-      const genres = JSON.parse(localStorage.getItem('genresData'));
-      genres.forEach(item => {
-        if (genre_ids.includes(item.id)) {
-          currentGenres.push(item.name);
-        }
-      });
-      poster_path = poster_path
-        ? 'https://image.tmdb.org/t/p/original' + poster_path
-        : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
-      return (
-        acc +
-        `<li class="gallery__item" id=${id} data-watched>
-          <div class="gallery__link">
-              <img class="gallery__image" src="${poster_path}" alt="${
-          title || name
-        }" width="394" height ="335"/> 
-        </div>
-        <div class="gallery__info">
-              <h2 class="gallery__title">${title || name}</h2>
-              <p class="gallery__genres">
-              <span class="gallery__genr"> ${
-                currentGenres.length > 3
-                  ? currentGenres.splice(0, 2).join(', ') + ', Others'
-                  : currentGenres.join(', ') || 'genre information missing'
-              } </span> |
-                  <span class="gallery__release">${
-                    release_date ? release_date.slice(0, 4) : 'no release date'
-                  }</span>
-                  <span class="gallery__rating">${vote_average.toFixed(1)}
-              </span>
-              </p>
-          </div>
-        </li>`
-      );
-    },
-    ''
-  );
-  galleryItem.innerHTML = markup;
-  return { results, page, total_pages };
-}
+//       poster_path = poster_path
+//         ? 'https://image.tmdb.org/t/p/original' + poster_path
+//         : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
+//       return (
+//         acc +
+//         `<li class="gallery__item" id=${id} data-trending>
+//           <div class="gallery__link">
+//               <img class="gallery__image" src="${poster_path}" alt="${
+//           title || name
+//         }" width="394" height ="335"/>
+//         </div>
+//         <div class="gallery__info">
+//               <h2 class="gallery__title">${title || name}</h2>
+//               <p class="gallery__genres">
+//               <span class="gallery__genr"> ${
+//                 currentGenres.length > 3
+//                   ? currentGenres.splice(0, 2).join(', ') + ', Others'
+//                   : currentGenres.join(', ') || 'genre information missing'
+//               }</span> |
+//                   <span class="gallery__release">${
+//                     release_date ? release_date.slice(0, 4) : 'no release date'
+//                   }</span>
+//                   <span class="gallery__rating">${vote_average.toFixed(1)}
+//               </span>
+//               </p>
+//           </div>
+//         </li>`
+//       );
+//     },
+//     ''
+//   );
+//   galleryItem.innerHTML = markup;
+//   return { results, page, total_pages };
+// }
 
-export function renderQueueMarkup(results, page, total_pages) {
-  const markup = results.reduce(
-    (
-      acc,
-      { id, poster_path, name, title, release_date, vote_average, genre_ids }
-    ) => {
-      const currentGenres = [];
-      const genres = JSON.parse(localStorage.getItem('genresData'));
-      genres.forEach(item => {
-        if (genre_ids.includes(item.id)) {
-          currentGenres.push(item.name);
-        }
-      });
-      poster_path = poster_path
-        ? 'https://image.tmdb.org/t/p/original' + poster_path
-        : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
-      return (
-        acc +
-        `<li class="gallery__item" id=${id} data-queue>
-          <div class="gallery__link">
-              <img class="gallery__image" src="${poster_path}" alt="${
-          title || name
-        }" width="394" height ="335"/> 
-        </div>
-        <div class="gallery__info">
-              <h2 class="gallery__title">${title || name}</h2>
-              <p class="gallery__genres">
-              <span class="gallery__genr"> ${
-                currentGenres.length > 3
-                  ? currentGenres.splice(0, 2).join(', ') + ', Others'
-                  : currentGenres.join(', ') || 'genre information missing'
-              } </span> |
-                  <span class="gallery__release">${
-                    release_date ? release_date.slice(0, 4) : 'no release date'
-                  }</span>
-                  <span class="gallery__rating">${vote_average.toFixed(1)}
-              </span>
-              </p>
-          </div>
-        </li>`
-      );
-    },
-    ''
-  );
-  galleryItem.innerHTML = markup;
-  return { results, page, total_pages };
-}
+// export function renderQueryMarkup(results, page, total_pages) {
+//   const markup = results.reduce(
+//     (
+//       acc,
+//       { id, poster_path, name, title, release_date, vote_average, genre_ids }
+//     ) => {
+//       const currentGenres = [];
+//       const genres = JSON.parse(localStorage.getItem('genresData'));
+//       genres.forEach(item => {
+//         if (genre_ids.includes(item.id)) {
+//           currentGenres.push(item.name);
+//         }
+//       });
+//       poster_path = poster_path
+//         ? 'https://image.tmdb.org/t/p/original' + poster_path
+//         : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
+//       return (
+//         acc +
+//         `<li class="gallery__item" id=${id} data-query>
+//           <div class="gallery__link">
+//               <img class="gallery__image" src="${poster_path}" alt="${
+//           title || name
+//         }" width="394" height ="335"/>
+//         </div>
+//         <div class="gallery__info">
+//               <h2 class="gallery__title">${title || name}</h2>
+//               <p class="gallery__genres">
+//               <span class="gallery__genr"> ${
+//                 currentGenres.length > 3
+//                   ? currentGenres.splice(0, 2).join(', ') + ', Others'
+//                   : currentGenres.join(', ') || 'genre information missing'
+//               } </span> |
+//                   <span class="gallery__release">${
+//                     release_date ? release_date.slice(0, 4) : 'no release date'
+//                   }</span>
+//                   <span class="gallery__rating">${vote_average.toFixed(1)}
+//               </span>
+//               </p>
+//           </div>
+//         </li>`
+//       );
+//     },
+//     ''
+//   );
+//   galleryItem.innerHTML = markup;
+//   return { results, page, total_pages };
+// }
+
+// export function renderWatchedMarkup(results, page, total_pages) {
+//   const markup = results.reduce(
+//     (
+//       acc,
+//       { id, poster_path, name, title, release_date, vote_average, genre_ids }
+//     ) => {
+//       const currentGenres = [];
+//       const genres = JSON.parse(localStorage.getItem('genresData'));
+//       genres.forEach(item => {
+//         if (genre_ids.includes(item.id)) {
+//           currentGenres.push(item.name);
+//         }
+//       });
+//       poster_path = poster_path
+//         ? 'https://image.tmdb.org/t/p/original' + poster_path
+//         : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
+//       return (
+//         acc +
+//         `<li class="gallery__item" id=${id} data-watched>
+//           <div class="gallery__link">
+//               <img class="gallery__image" src="${poster_path}" alt="${
+//           title || name
+//         }" width="394" height ="335"/>
+//         </div>
+//         <div class="gallery__info">
+//               <h2 class="gallery__title">${title || name}</h2>
+//               <p class="gallery__genres">
+//               <span class="gallery__genr"> ${
+//                 currentGenres.length > 3
+//                   ? currentGenres.splice(0, 2).join(', ') + ', Others'
+//                   : currentGenres.join(', ') || 'genre information missing'
+//               } </span> |
+//                   <span class="gallery__release">${
+//                     release_date ? release_date.slice(0, 4) : 'no release date'
+//                   }</span>
+//                   <span class="gallery__rating">${vote_average.toFixed(1)}
+//               </span>
+//               </p>
+//           </div>
+//         </li>`
+//       );
+//     },
+//     ''
+//   );
+//   galleryItem.innerHTML = markup;
+//   return { results, page, total_pages };
+// }
+
+// export function renderQueueMarkup(results, page, total_pages) {
+//   const markup = results.reduce(
+//     (
+//       acc,
+//       { id, poster_path, name, title, release_date, vote_average, genre_ids }
+//     ) => {
+//       const currentGenres = [];
+//       const genres = JSON.parse(localStorage.getItem('genresData'));
+//       genres.forEach(item => {
+//         if (genre_ids.includes(item.id)) {
+//           currentGenres.push(item.name);
+//         }
+//       });
+//       poster_path = poster_path
+//         ? 'https://image.tmdb.org/t/p/original' + poster_path
+//         : 'https://i.ibb.co/SwmHkLf/zaglushka.jpg';
+//       return (
+//         acc +
+//         `<li class="gallery__item" id=${id} data-queue>
+//           <div class="gallery__link">
+//               <img class="gallery__image" src="${poster_path}" alt="${
+//           title || name
+//         }" width="394" height ="335"/>
+//         </div>
+//         <div class="gallery__info">
+//               <h2 class="gallery__title">${title || name}</h2>
+//               <p class="gallery__genres">
+//               <span class="gallery__genr"> ${
+//                 currentGenres.length > 3
+//                   ? currentGenres.splice(0, 2).join(', ') + ', Others'
+//                   : currentGenres.join(', ') || 'genre information missing'
+//               } </span> |
+//                   <span class="gallery__release">${
+//                     release_date ? release_date.slice(0, 4) : 'no release date'
+//                   }</span>
+//                   <span class="gallery__rating">${vote_average.toFixed(1)}
+//               </span>
+//               </p>
+//           </div>
+//         </li>`
+//       );
+//     },
+//     ''
+//   );
+//   galleryItem.innerHTML = markup;
+//   return { results, page, total_pages };
+// }
