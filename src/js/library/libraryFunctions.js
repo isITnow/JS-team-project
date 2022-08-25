9dc2536e5bfd7c2d28598d56ec9885d95fe636e6
Roman Serediuk <box4rom@gmail.com>
2022-08-26 01:10:40 +0300

    crazy voodoo

--- src/js/library/libraryFunctions.js
+++ src/js/library/libraryFunctions.js
@@ -1,28 +1,35 @@
 import { renderGalleryMarkup } from '../home/renderGalleryMarkup';
 import { renderMovieMarkup } from '../renderModal';
+const galleryList = document.querySelector('.gallery__list');
 
-const btnsLib = document.querySelector('.buttons-list')
-btnsLib.addEventListener('click', onLibBtnClick)
+const btnsLib = document.querySelector('.buttons-list');
+btnsLib.addEventListener('click', onLibBtnClick);
 
-
-export function libraryMovieCreator(){
-    if(!localStorage.getItem('library')){
-return;
-// заглушка
+export function libraryMovieCreator() {
+  if (!localStorage.getItem('library')) {
+    return;
+    // заглушка
+  }
+  const parsedLib = JSON.parse(localStorage.getItem('library'));
+  renderGalleryMarkup('', parsedLib, '', 'data-library');
 }
-const parsedLib = JSON.parse(localStorage.getItem('library'))
-renderGalleryMarkup(parsedLib, 'data-library')
 
-}
+function onLibBtnClick(e) {
+  if (e.target.classList.contains('js-watchedBtn')) {
+    if (localStorage.getItem('watchedMovies')) {
+      const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'));
+      renderGalleryMarkup('', parsedWatched, '', 'data-watched');
+    } else {
+      galleryList.innerHTML = 'ХЕР ВАМ!';
+    }
+  }
 
-
-function onLibBtnClick(e){
-if(e.target.classList.contains('js-watchedBtn')){
-    const parsedWatched = JSON.parse(localStorage.getItem('watchedMovies'))
-    renderGalleryMarkup(parsedWatched, 'data-watched')
-}
-if(e.target.classList.contains('js-queueBtn')){
-    const parsedQueue = JSON.parse(localStorage.getItem('queueMovies'))
-    renderGalleryMarkup(parsedQueue, 'data-queue')
-}
+  if (e.target.classList.contains('js-queueBtn')) {
+    if (localStorage.getItem('queueMovies')) {
+      const parsedQueue = JSON.parse(localStorage.getItem('queueMovies'));
+      renderGalleryMarkup('', parsedQueue, '', 'data-queue');
+    } else {
+      galleryList.innerHTML = 'ХЕР ВАМ!';
+    }
+  }
 }
--- src/js/modal/modalBtnFunction.js
+++ src/js/modal/modalBtnFunction.js
@@ -2,6 +2,7 @@
 // import { addQueueToLocalStorage } from '../library/localStorage';
 
 export function onAddToWatched(data, storageKey) {
+  console.log('CLICK');
   setToLocalStorage(data, storageKey);
 }
 
@@ -24,6 +25,24 @@ function setToLocalStorage(data, storageKey) {
   if (!arr.some(item => item.id === data.id)) {
     arr.push(data);
     localStorage.setItem(storageKey, JSON.stringify(arr));
+  }
+}
+
+export function modalBtnsStatusCheck(currentMovie) {
+  const watchedArr = JSON.parse(localStorage.getItem('watchedMovies'));
+  const queueArr = JSON.parse(localStorage.getItem('queueMovies'));
+  if (localStorage.getItem('watchedMovies')) {
+    if (watchedArr.some(item => item.id === currentMovie.id)) {
+      const watchedBtn = document.querySelector('.js-addToWatched');
+      watchedBtn.textContent = 'Remove from watched';
+    }
+  }
+
+  if (localStorage.getItem('queueMovies')) {
+    if (queueArr.some(item => item.id === currentMovie.id)) {
+      const queueBtn = document.querySelector('.js-addToQueue');
+      queueBtn.textContent = 'Remove from queue';
+    }
   }
 }
 
